e4c375d47d0d2fb525d304557271d2a5c6dbc50f
Crash80824 <119785272+Crash80824@users.noreply.github.com>
2023-03-07 02:15:43 +0800

    添加楼层信息

--- shire_article_saver.js
+++ shire_article_saver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         shire article saver
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  download shire thread content
 // @author       Crash
 // @match        https://www.shireyishunjian.com/main/forum.php?mod=viewthread*
@@ -13,13 +13,46 @@
 (function () {
     'use strict';
 
-    const title_info = document.getElementsByClassName('ts')[0].children;
-    const title_name = title_info[0].textContent.replace('[', '【').replace(']', '】') + title_info[1].textContent;
-    const auth_uid = document.getElementsByClassName('authi')[0].getInnerHTML().match(/uid=\d*/)[0].split('=')[1];
+    function getPostlistTitle(postlist) {
+        const title_info = postlist.getElementsByClassName('ts')[0].children;
+        let title_name = title_info[0].textContent.replace('[', '【').replace(']', '】') + title_info[1].textContent;
+        return title_name;
+    }
 
-    if (!location.href.match(/page=([2-9]|[1-9]\d+)/)) {
+    function getPostInfo(post) {
+        const post_id = post.id.split('_')[1];
+        const post_auth = post.getElementsByClassName('authi')[0].firstElementChild.text;
+        const post_auth_id = post.getElementsByClassName('authi')[0].firstElementChild.href.split('uid=')[1];
+        const sub_time = post.getElementsByClassName('authi')[1].querySelector('[id^=authorposton]').textContent;
+        const post_url = document.location.origin + '/main/forum.php?mod=redirect&goto=findpost&ptid=' + thread_id + '&pid=' + post_id;
+        const post_content = post.getElementsByClassName('t_f')[0].textContent;
+
+        return { 'post_id': post_id, 'post_auth': post_auth, 'post_auth_id': post_auth_id, 'sub_time': sub_time, 'post_url': post_url, 'post_content': post_content };
+    }
+
+    const postlist = document.getElementById('postlist');
+    const post_in_page = postlist.querySelectorAll('[class^=post_gender]');
+    const thread_id = document.getElementsByClassName('plc ptm pbn vwthd')[0].lastElementChild.lastElementChild.href.split('tid=')[1];
+    const first_post_floor = post_in_page[0].getElementsByClassName('pi')[1].getElementsByTagName('strong')[0];
+    const is_fisrt_page = first_post_floor.textContent.includes('楼主');
+
+    let thread_auth_name = '';
+    let thread_auth_id = '';
+    if (is_fisrt_page) {
+        const first_post_info = getPostInfo(post_in_page[0]);
+        thread_auth_name = first_post_info.post_auth;
+        thread_auth_id = first_post_info.post_auth_id;
+    }
+    else {
+        thread_auth_name = document.getElementById('tath').firstElementChild.title;
+        thread_auth_id = document.getElementById('tath').firstElementChild.href.split('uid=')[1];
+    }
+
+    let title_name = getPostlistTitle(postlist);
+
+    if (is_fisrt_page) {
         let filename = title_name;
-        let content = document.getElementsByClassName('t_f')[0].textContent;
+        let content = post_in_page[0].getElementsByClassName('t_f')[0].textContent;
 
         const buffer = new TextEncoder().encode(content).buffer;
         const blob = new Blob([buffer], { type: 'text/plain;base64' });
@@ -27,7 +60,7 @@
 
         reader.readAsDataURL(blob);
         reader.onload = (event) => {
-            const download_pos = document.getElementsByClassName('pi')[1].getElementsByTagName('strong')[0];
+            const download_pos = first_post_floor;
             const download_href = document.createElement('a');
             download_href.innerHTML = '保存主楼';
             download_href.href = event.target.result;
@@ -36,12 +69,18 @@
         };
     }
 
-    if (location.href.indexOf('authorid=' + auth_uid) > 0) {
-        let pageid = location.href.match(/page=\d*/)[0].split('=')[1];
+    if (location.href.includes('authorid=' + thread_auth_id)) {
+        const pageid = location.href.match(/page=\d*/)[0].split('=')[1];
         let filename = title_name + ' - ' + pageid;
         let content = '';
-        for (let thread of document.getElementsByClassName('t_f')) {
-            content += thread.textContent;
+        for (let i = 0; i < post_in_page.length; i++) {
+            const post_info = getPostInfo(post_in_page[i]);
+            content+='<----------------\n';
+            content += '//作者：' + post_info.post_auth + '(UID: ' + post_info.post_auth_id + ')\n';
+            content += '//链接：' + post_info.post_url + '\n';
+            content += '//时间：' + post_info.sub_time + '\n';
+            content += post_info.post_content;
+            content+='\n---------------->\n';
         }
 
         const buffer = new TextEncoder().encode(content).buffer;
