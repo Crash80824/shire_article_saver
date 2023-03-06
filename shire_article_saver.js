1b301280b56b65ca27d834a6658300cd6ad818bd
Crash80824 <119785272+Crash80824@users.noreply.github.com>
2023-03-07 03:23:29 +0800

    调整quote的显示

--- shire_article_saver.js
+++ shire_article_saver.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         shire article saver
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
-// @description  download shire thread content
+// @version      0.1.3
+// @description  Download shire thread content.
 // @author       Crash
 // @match        https://www.shireyishunjian.com/main/forum.php?mod=viewthread*
 // @match        https://www.shishirere.com/main/forum.php?mod=viewthread*
@@ -12,6 +12,26 @@
 
 (function () {
     'use strict';
+
+    function getPostContent(post) {
+        let childrenNodes = post.childNodes;
+        let content = '';
+        for (let i = 0; i < childrenNodes.length; i++) {
+            if (childrenNodes[i].className == 'quote') {
+                content += '<<<\n';
+                let quote_href = childrenNodes[i].getElementsByTagName('a')[0];
+                let origin_quote = quote_href.innerText;
+                quote_href.innerText += ' 链接: ' + quote_href.href;
+                content += childrenNodes[i].textContent + '\n';
+                quote_href.innerText = origin_quote;
+                content += '>>>\n';
+            }
+            else {
+                content += childrenNodes[i].textContent;
+            }
+        }
+        return content;
+    }
 
     function getPostlistTitle(postlist) {
         const title_info = postlist.getElementsByClassName('ts')[0].children;
@@ -25,7 +45,7 @@
         const post_auth_id = post.getElementsByClassName('authi')[0].firstElementChild.href.split('uid=')[1];
         const sub_time = post.getElementsByClassName('authi')[1].querySelector('[id^=authorposton]').textContent;
         const post_url = document.location.origin + '/main/forum.php?mod=redirect&goto=findpost&ptid=' + thread_id + '&pid=' + post_id;
-        const post_content = post.getElementsByClassName('t_f')[0].textContent;
+        const post_content = getPostContent(post.getElementsByClassName('t_f')[0]);
 
         return { 'post_id': post_id, 'post_auth': post_auth, 'post_auth_id': post_auth_id, 'sub_time': sub_time, 'post_url': post_url, 'post_content': post_content };
     }
@@ -52,7 +72,13 @@
 
     if (is_fisrt_page) {
         let filename = title_name;
-        let content = post_in_page[0].getElementsByClassName('t_f')[0].textContent;
+        let content = '';
+
+        const post_info = getPostInfo(post_in_page[0]);
+        content += '//作者：' + post_info.post_auth + '(UID: ' + post_info.post_auth_id + ')\n';
+        content += '//链接：' + post_info.post_url + '\n';
+        content += '//时间：' + post_info.sub_time + '\n';
+        content += post_info.post_content;
 
         const buffer = new TextEncoder().encode(content).buffer;
         const blob = new Blob([buffer], { type: 'text/plain;base64' });
@@ -75,12 +101,12 @@
         let content = '';
         for (let i = 0; i < post_in_page.length; i++) {
             const post_info = getPostInfo(post_in_page[i]);
-            content+='<----------------\n';
+            content += '<----------------\n';
             content += '//作者：' + post_info.post_auth + '(UID: ' + post_info.post_auth_id + ')\n';
             content += '//链接：' + post_info.post_url + '\n';
             content += '//时间：' + post_info.sub_time + '\n';
             content += post_info.post_content;
-            content+='\n---------------->\n';
+            content += '\n---------------->\n';
         }
 
         const buffer = new TextEncoder().encode(content).buffer;
