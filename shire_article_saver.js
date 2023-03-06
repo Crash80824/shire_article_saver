ebd4246831d3220bb4a3af1c3766dfb5931868d9
Crash80824 <119785272+Crash80824@users.noreply.github.com>
2023-03-06 22:21:40 +0800

    只在第一页上显示下载按钮

--- shire_article_saver.js
+++ shire_article_saver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         shire article saver
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.0.1
 // @description  download shire thread content
 // @author       Crash
 // @match        https://www.shireyishunjian.com/main/forum.php?mod=viewthread*
@@ -12,21 +12,23 @@
 
 (function () {
     'use strict';
-    const title_info = document.getElementsByClassName('ts')[0].children;
-    let filename = title_info[0].textContent.replace('[', '【').replace(']', '】') + title_info[1].textContent;
-    let content = document.getElementsByClassName('t_f')[0].textContent;
+    if (!location.href.match(/page=([2-9]|[1-9]\d+)/)) {
+        const title_info = document.getElementsByClassName('ts')[0].children;
+        let filename = title_info[0].textContent.replace('[', '【').replace(']', '】') + title_info[1].textContent;
+        let content = document.getElementsByClassName('t_f')[0].textContent;
 
-    const buffer = new TextEncoder().encode(content).buffer;
-    const blob = new Blob([buffer], { type: 'text/plain;base64' });
-    const reader = new FileReader();
-    reader.readAsDataURL(blob);
+        const buffer = new TextEncoder().encode(content).buffer;
+        const blob = new Blob([buffer], { type: 'text/plain;base64' });
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
 
-    reader.onload = (event) => {
-        const download_pos = document.getElementsByClassName('pi')[1].getElementsByTagName('strong')[0];
-        const download_href = document.createElement('a');
-        download_href.innerHTML = '下载';
-        download_href.href = event.target.result;
-        download_href.download = filename;
-        download_pos.appendChild(download_href);
-      };
+        reader.onload = (event) => {
+            const download_pos = document.getElementsByClassName('pi')[1].getElementsByTagName('strong')[0];
+            const download_href = document.createElement('a');
+            download_href.innerHTML = '下载';
+            download_href.href = event.target.result;
+            download_href.download = filename;
+            download_pos.appendChild(download_href);
+        };
+    }
 })();
