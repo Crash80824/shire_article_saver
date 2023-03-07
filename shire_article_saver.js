5a1cea935f9a03e3e6a50f7544dbd9a0c5c076e9
Crash80824 <119785272+Crash80824@users.noreply.github.com>
2023-03-07 17:51:28 +0800

    分割线格式

--- shire_article_saver.js
+++ shire_article_saver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         shire article saver
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  Download shire thread content.
 // @author       Crash
 // @match        https://www.shireyishunjian.com/main/forum.php?mod=viewthread*
@@ -14,25 +14,31 @@
     'use strict';
 
     function getPostContent(post) {
-        let childrenNodes = post.childNodes;
+        const tf = post.getElementsByClassName('t_f')[0]
+        let childrenNodes = tf.childNodes;
         let content = '';
         for (let i = 0; i < childrenNodes.length; i++) {
-            if (childrenNodes[i].className == 'quote') {
-                content += '<<<\n';
-                let quote_href = childrenNodes[i].getElementsByTagName('a')[0];
-                if (quote_href) {
-                    let origin_quote = quote_href.innerText;
-                    quote_href.innerText += ' ' + quote_href.href.match(/pid=\d*/)[0].replace('pid=', 'PID:');
-                    content += childrenNodes[i].textContent + '\n';
-                    quote_href.innerText = origin_quote;
-                }
-                else {
-                    content += childrenNodes[i].textContent + '\n'
-                }
-                content += '>>>\n';
-            }
-            else {
-                content += childrenNodes[i].textContent;
+            const child = childrenNodes[i];
+            switch (child.tagName + '.' + child.className) {
+                case 'DIV.quote':
+                    content += '<<<\n';
+                    let quote_href = child.getElementsByTagName('a')[0];
+                    if (quote_href) {
+                        let origin_quote = quote_href.innerText;
+                        quote_href.innerText += ' ' + quote_href.href.match(/pid=\d*/)[0].replace('pid=', 'PID: ');
+                        content += child.textContent + '\n';
+                        quote_href.innerText = origin_quote;
+                    }
+                    else {
+                        content += child.textContent + '\n'
+                    }
+                    content += '>>>\n';
+                    break;
+                case 'HR.l':
+                    content += '++++++++\n';
+                    break;
+                default:
+                    content += childrenNodes[i].textContent;
             }
         }
         return content;
@@ -50,7 +56,7 @@
         const post_auth_id = post.getElementsByClassName('authi')[0].firstElementChild.href.split('uid=')[1];
         const sub_time = post.getElementsByClassName('authi')[1].querySelector('[id^=authorposton]').textContent;
         const post_url = document.location.origin + '/main/forum.php?mod=redirect&goto=findpost&ptid=' + thread_id + '&pid=' + post_id;
-        const post_content = getPostContent(post.getElementsByClassName('t_f')[0]);
+        const post_content = getPostContent(post);
 
         return { 'post_id': post_id, 'post_auth': post_auth, 'post_auth_id': post_auth_id, 'sub_time': sub_time, 'post_url': post_url, 'post_content': post_content };
     }
@@ -83,7 +89,7 @@
         let content = file_info;
 
         const post_info = getPostInfo(post_in_page[0]);
-        content += '//' + post_info.post_auth + '(UID:' + post_info.post_auth_id + ') ' + post_info.sub_time + '\n';
+        content += '//' + post_info.post_auth + '(UID: ' + post_info.post_auth_id + ') ' + post_info.sub_time + '\n';
         content += '//PID:' + post_info.post_id + '\n';
         content += post_info.post_content;
 
@@ -109,7 +115,7 @@
         for (let i = 0; i < post_in_page.length; i++) {
             const post_info = getPostInfo(post_in_page[i]);
             content += '<----------------\n';
-            content += '//' + post_info.post_auth + '(UID:' + post_info.post_auth_id + ') ' + post_info.sub_time + '\n';
+            content += '//' + post_info.post_auth + '(UID: ' + post_info.post_auth_id + ') ' + post_info.sub_time + '\n';
             content += '//PID:' + post_info.post_id + '\n';
             content += post_info.post_content;
             content += '\n---------------->\n';
