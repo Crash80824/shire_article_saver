a3b4998e4626f7b7cdf00619e4a1c574de87e5a5
Crash80824 <119785272+Crash80824@users.noreply.github.com>
2023-03-07 18:51:02 +0800

    全面使用querySelector

--- shire_article_saver.js
+++ shire_article_saver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         shire article saver
 // @namespace    http://tampermonkey.net/
-// @version      0.1.5
+// @version      0.1.5.1
 // @description  Download shire thread content.
 // @author       Crash
 // @match        https://www.shireyishunjian.com/main/forum.php?mod=viewthread*
@@ -13,8 +13,11 @@
 (function () {
     'use strict';
 
-    function getPostContent(post) {
-        const tf = post.getElementsByClassName('t_f')[0]
+    const $ = (selector, parent = document) => parent.querySelector(selector);
+    const $$ = (selector, parent = document) => parent.querySelectorAll(selector);
+
+    function getPostContent(pid) {
+        const tf = $('#postmessage_' + pid);
         let childrenNodes = tf.childNodes;
         let content = '';
         for (let i = 0; i < childrenNodes.length; i++) {
@@ -22,7 +25,7 @@
             switch (child.tagName + '.' + child.className) {
                 case 'DIV.quote':
                     content += '<<<\n';
-                    let quote_href = child.getElementsByTagName('a')[0];
+                    let quote_href = $('td > div > blockquote > font > a', child);
                     if (quote_href) {
                         let origin_quote = quote_href.innerText;
                         quote_href.innerText += ' ' + quote_href.href.match(/pid=\d*/)[0].replace('pid=', 'PID: ');
@@ -38,33 +41,27 @@
                     content += '++++++++\n';
                     break;
                 default:
-                    content += childrenNodes[i].textContent;
+                    content += child.textContent;
             }
         }
         return content;
     }
 
-    function getPostlistTitle(postlist) {
-        const title_info = postlist.getElementsByClassName('ts')[0].children;
-        let title_name = title_info[0].textContent.replace('[', '【').replace(']', '】') + title_info[1].textContent;
-        return title_name;
-    }
-
     function getPostInfo(post) {
         const post_id = post.id.split('_')[1];
-        const post_auth = post.getElementsByClassName('authi')[0].firstElementChild.text;
-        const post_auth_id = post.getElementsByClassName('authi')[0].firstElementChild.href.split('uid=')[1];
-        const sub_time = post.getElementsByClassName('authi')[1].querySelector('[id^=authorposton]').textContent;
+        const post_auth = $('#favatar' + post_id + ' > div.pi > div > a', post).text;
+        const post_auth_id = $('#favatar' + post_id + ' > div.pi > div > a', post).href.split('uid=')[1];
+        const sub_time = $('[id^=authorposton]', post).textContent;
         const post_url = document.location.origin + '/main/forum.php?mod=redirect&goto=findpost&ptid=' + thread_id + '&pid=' + post_id;
-        const post_content = getPostContent(post);
+        const post_content = getPostContent(post_id);
 
         return { 'post_id': post_id, 'post_auth': post_auth, 'post_auth_id': post_auth_id, 'sub_time': sub_time, 'post_url': post_url, 'post_content': post_content };
     }
 
 
-    const postlist = document.getElementById('postlist');
-    const post_in_page = postlist.querySelectorAll('[class^=post_gender]');
-    const thread_id = document.getElementsByClassName('plc ptm pbn vwthd')[0].lastElementChild.lastElementChild.href.split('tid=')[1];
+    const postlist = $('#postlist');
+    const post_in_page = $$('[class^=post_gender]', postlist);
+    const thread_id = $('#postlist > table:nth-child(1) > tbody > tr > td.plc.ptm.pbn.vwthd > span > a').href.split('tid=')[1];
     const is_fisrt_page = !location.href.match(/page=([2-9]|[1-9]\d+)/);
 
     let thread_auth_name = '';
@@ -75,12 +72,12 @@
         thread_auth_id = first_post_info.post_auth_id;
     }
     else {
-        thread_auth_name = document.getElementById('tath').firstElementChild.title;
-        thread_auth_id = document.getElementById('tath').firstElementChild.href.split('uid=')[1];
+        thread_auth_name = $('#tath > a:nth-child(1)').title;
+        thread_auth_id = $('#tath > a:nth-child(1)').href.split('uid=')[1];
     }
 
 
-    let title_name = getPostlistTitle(postlist);
+    let title_name = $('#thread_subject').parentNode.textContent.replace('\n', '').replace('[', '【').replace(']', '】');
     let file_info = 'Link: ' + location.href + '\n****************\n';
 
 
@@ -99,7 +96,7 @@
 
         reader.readAsDataURL(blob);
         reader.onload = (event) => {
-            const download_pos = post_in_page[0].getElementsByClassName('pi')[1].getElementsByTagName('strong')[0];
+            const download_pos = $('table > tbody > tr:nth-child(1) > td.plc > div.pi > strong', post_in_page[0]);
             const download_href = document.createElement('a');
             download_href.innerHTML = '保存主楼';
             download_href.href = event.target.result;
@@ -127,9 +124,9 @@
 
         reader.readAsDataURL(blob);
         reader.onload = (event) => {
-            const download_pos = document.getElementsByClassName('plc ptm pbn vwthd')[0].children[0];
+            const download_pos = $('#postlist > table:nth-child(1) > tbody > tr > td.plc.ptm.pbn.vwthd > div')
             const download_href = document.createElement('a');
-            download_href.innerHTML = '保存本页';
+            download_href.innerHTML = '保存本帖';
             download_href.href = event.target.result;
             download_href.download = filename;
             download_pos.appendChild(download_href);
